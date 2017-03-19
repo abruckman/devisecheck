be99164cca41af607c55ace169b7a23f2a679262
abruckman <abruckman09@gmail.com>
2017-03-18 20:11:03 -0700

    add oauth call

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,16 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def all
+    p request.env["omniauth.auth"]
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      sign_in_and_redirect user, notice:"signed in!"
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url, notice: "not signed in"
+    end
+  end
+
+  # p flash
+
+  alias_method :facebook, :all
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,32 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.email
+    end
+  end
+
+  def new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: :true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
+
+  # def password_
+
 end
